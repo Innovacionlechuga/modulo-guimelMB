403b4516abd989df7b680c62d18cef4e5d1cea6e
AldoIs Mendoza <aldois.rov@gmail.com>
2020-01-19 20:33:19 -0800

    Basic Frontend React

---
+++ src/App.js
@@ -0,0 +1,60 @@
+﻿import React, { Component } from 'react';
+import logo from './logo.svg';
+import 'nes.css/css/nes.css';
+
+import './App.css';
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTask: "",
+            tasks: [],
+            
+        }
+    }
+
+    handleTaskChange = (event) => {
+        this.setState({
+            newTask: event.target.value,
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let newTasks = this.state.tasks;
+        newTasks.push(this.state.newTask);
+        this.setState({
+            newTask: "",
+            tasks: newTasks
+        })
+    }
+    
+    render() {
+        return (
+            <div className="container">
+                <h1 className="title">
+                   Lista de tareas (To-Do)
+               <span aria-label="emoji" role="img">🐞</span>
+                </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.newTask} onChange={this.handleTaskChange} type="text" className="new-task" />
+                    <br />
+                    <button className="nes-btn is-primary">Cuack</button>
+                </form>
+                
+                {
+                    this.state.tasks.map(task => 
+                        <div className="task-container" key={task.toString()}>
+                            <label>
+                                <input type="checkbox"  className="nes-checkbox  task"/>
+                                <span>{task}</span>
+                            </label>
+                        </div>
+                        )
+                }
+
+            </div>
+        );
+    }
+}
+
+export default App;
